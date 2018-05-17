cff7be4cd8fd9a09e697e3c476db0e035b915563
zhaoq0563 <zhaoq0563@gmail.com>
2018-05-17 12:05:01 -0700

    fix the folder problem

--- util/install.sh
+++ util/install.sh
@@ -142,7 +142,7 @@ function mn_deps {
     pip install typing
 
     echo "Installing Mininet core"
-    pushd $MININET_DIR/mininet-wifi
+    pushd $MININET_DIR/mininet-wifi-testbed
     sudo make install
     popd
 }
@@ -152,17 +152,17 @@ function wifi_deps {
     echo "Installing Mininet-WiFi dependencies"
     $install wireless-tools rfkill python-numpy python-scipy pkg-config \
             python-matplotlib libnl-3-dev libnl-genl-3-dev libssl-dev make libevent-dev patch
-    pushd $MININET_DIR/mininet-wifi
+    pushd $MININET_DIR/mininet-wifi-testbed
     git submodule update --init --recursive
-    pushd $MININET_DIR/mininet-wifi/hostap
-    patch -p0 < $MININET_DIR/mininet-wifi/util/hostap-patches/config.patch
-    pushd $MININET_DIR/mininet-wifi/hostap/hostapd
+    pushd $MININET_DIR/mininet-wifi-testbed/hostap
+    patch -p0 < $MININET_DIR/mininet-wifi-testbed/util/hostap-patches/config.patch
+    pushd $MININET_DIR/mininet-wifi-testbed/hostap/hostapd
     cp defconfig .config
     sudo make && make install
-    pushd $MININET_DIR/mininet-wifi/hostap/wpa_supplicant
+    pushd $MININET_DIR/mininet-wifi-testbed/hostap/wpa_supplicant
     cp defconfig .config
     sudo make && make install
-    pushd $MININET_DIR/mininet-wifi/iw
+    pushd $MININET_DIR/mininet-wifi-testbed/iw
     sudo make && make install
     cd $BUILD_DIR
     if [ -d mac80211_hwsim_mgmt ]; then
@@ -200,7 +200,7 @@ function of {
     cd $BUILD_DIR/openflow
 
     # Patch controller to handle more than 16 switches
-    patch -p1 < $MININET_DIR/mininet-wifi/util/openflow-patches/controller.patch
+    patch -p1 < $MININET_DIR/mininet-wifi-testbed/util/openflow-patches/controller.patch
 
     # Resume the install:
     ./boot.sh
@@ -262,7 +262,7 @@ function install_wireshark {
     # Copy coloring rules: OF is white-on-blue:
     echo "Optionally installing wireshark color filters"
     mkdir -p $HOME/.wireshark
-    cp -n $MININET_DIR/mininet-wifi/util/colorfilters $HOME/.wireshark
+    cp -n $MININET_DIR/mininet-wifi-testbed/util/colorfilters $HOME/.wireshark
 
     echo "Checking Wireshark version"
     WSVER=`wireshark -v | egrep -o '[0-9\.]+' | head -1`
@@ -494,9 +494,9 @@ function nox {
 
     # Apply patches
     git checkout -b tutorial-destiny
-    git am $MININET_DIR/mininet-wifi/util/nox-patches/*tutorial-port-nox-destiny*.patch
+    git am $MININET_DIR/mininet-wifi-testbed/util/nox-patches/*tutorial-port-nox-destiny*.patch
     if [ "$DIST" = "Ubuntu" ] && version_ge $RELEASE 12.04; then
-        git am $MININET_DIR/mininet-wifi/util/nox-patches/*nox-ubuntu12-hacks.patch
+        git am $MININET_DIR/mininet-wifi-testbed/util/nox-patches/*nox-ubuntu12-hacks.patch
     fi
 
     # Build
@@ -561,17 +561,17 @@ function pox {
 function iw {
     echo "Installing iw..."
     $install wireless-tools python-numpy python-scipy pkg-config python-matplotlib libnl-3-dev libnl-genl-3-dev libssl-dev patch
-    pushd $MININET_DIR/mininet-wifi
+    pushd $MININET_DIR/mininet-wifi-testbed
     git submodule update --init --recursive
-    pushd $MININET_DIR/mininet-wifi/hostap
-    patch -p0 < $MININET_DIR/mininet-wifi/util/hostap-patches/config.patch
-    pushd $MININET_DIR/mininet-wifi/hostap/hostapd
+    pushd $MININET_DIR/mininet-wifi-testbed/hostap
+    patch -p0 < $MININET_DIR/mininet-wifi-testbed/util/hostap-patches/config.patch
+    pushd $MININET_DIR/mininet-wifi-testbed/hostap/hostapd
     cp defconfig .config
     sudo make && make install
-    pushd $MININET_DIR/mininet-wifi/hostap/wpa_supplicant
+    pushd $MININET_DIR/mininet-wifi-testbed/hostap/wpa_supplicant
     cp defconfig .config
     sudo make && make install
-    pushd $MININET_DIR/mininet-wifi/iw
+    pushd $MININET_DIR/mininet-wifi-testbed/iw
     sudo make && make install
     #popd
 }
